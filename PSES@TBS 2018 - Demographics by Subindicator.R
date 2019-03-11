4c37e6587ccd392aabde69598159e18d052dffe7
obyrneb <byron.obyrne@gmail.com>
2019-03-10 22:06:58 -0400

    Integrated PSES 2017 and 2018 sector results

--- PSES@TBS 2018 - Demographics by Subindicator.R
+++ PSES@TBS 2018 - Demographics by Subindicator.R
@@ -7,37 +7,37 @@ library(tidyverse)
 mainDir <- getwd()
 dataDir <- "datasets"
 plotDir <- "plots"
-ss1File <- file.path(mainDir,dataDir,"pses2018_ss1.csv")
-ss2File <- file.path(mainDir,dataDir,"pses2018_ss2.csv")
-ss3File <- file.path(mainDir,dataDir,"pses2018_ss3.csv")
-ss4File <- file.path(mainDir,dataDir,"pses2018_ss4.csv")
-ss5File <- file.path(mainDir,dataDir,"pses2018_ss5.csv")
-ss1URL <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-1_Sous-ensemble-1.csv"
-ss2URL <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-2_Sous-ensemble-2.csv"
-ss3URL <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-3_Sous-ensemble-3.csv"
-ss4URL <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-4_Sous-ensemble-4.csv"
-ss5URL <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-5_Sous-ensemble-5.csv" # Due for release in March 2018
+ss1File_2018 <- file.path(mainDir,dataDir,"pses2018_ss1.csv")
+ss2File_2018 <- file.path(mainDir,dataDir,"pses2018_ss2.csv")
+ss3File_2018 <- file.path(mainDir,dataDir,"pses2018_ss3.csv")
+ss4File_2018 <- file.path(mainDir,dataDir,"pses2018_ss4.csv")
+ss5File_2018 <- file.path(mainDir,dataDir,"pses2018_ss5.csv")
+ss1URL_2018 <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-1_Sous-ensemble-1.csv"
+ss2URL_2018 <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-2_Sous-ensemble-2.csv"
+ss3URL_2018 <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-3_Sous-ensemble-3.csv"
+ss4URL_2018 <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-4_Sous-ensemble-4.csv"
+ss5URL_2018 <- "https://www.canada.ca/content/dam/tbs-sct/documents/datasets/pses-saff/2018/2018_PSES_SAFF_Subset-5_Sous-ensemble-5.csv" # Due for release in March 2018
 
 ifelse(!dir.exists(file.path(mainDir, dataDir)), dir.create(file.path(mainDir, dataDir)), FALSE)
 ifelse(!dir.exists(file.path(mainDir, plotDir)), dir.create(file.path(mainDir, plotDir)), FALSE)
 
-if(!file.exists(c(ss1File,ss2File,ss3File,ss4File,ss5File))) { # Add ss5File here once released
-  download.file(ss1URL,ss1File)
-  download.file(ss2URL,ss2File)
-  download.file(ss3URL,ss3File)
-  download.file(ss4URL,ss4File)
-  download.file(ss5URL,ss5File)
+if(!file.exists(c(ss1File_2018,ss2File_2018,ss3File_2018,ss4File_2018,ss5File_2018))) {
+  download.file(ss1URL_2018,ss1File_2018)
+  download.file(ss2URL_2018,ss2File_2018)
+  download.file(ss3URL_2018,ss3File_2018)
+  download.file(ss4URL_2018,ss4File_2018)
+  download.file(ss5URL_2018,ss5File_2018)
 }
 
 if(!exists("pses2018")) {
-  ss1 <- read.csv(ss1File, na.strings = "9999")
-  ss2 <- read.csv(ss2File, na.strings = "9999")
-  ss3 <- read.csv(ss3File, na.strings = "9999")
-  ss4 <- read.csv(ss4File, na.strings = "9999")
-  ss5 <- read.csv(ss5File, na.strings = "9999")
+  ss1_2018 <- read.csv(ss1File_2018, na.strings = "9999")
+  ss2_2018 <- read.csv(ss2File_2018, na.strings = "9999")
+  ss3_2018 <- read.csv(ss3File_2018, na.strings = "9999")
+  ss4_2018 <- read.csv(ss4File_2018, na.strings = "9999")
+  ss5_2018 <- read.csv(ss5File_2018, na.strings = "9999")
   indicatorMap <- read.csv(file.path(mainDir,dataDir,"PSES2018_Indicator_Mapping.csv")) %>%
     select(-TITLE_E,-TITLE_F)
-  pses2018 <- bind_rows(ss1,ss2,ss3,ss4,ss5) %>%
+  pses2018 <- bind_rows(ss1_2018,ss2_2018,ss3_2018,ss4_2018,ss5_2018) %>%
     left_join(indicatorMap, by = "QUESTION")
 }
 #------------
